d59421f4c5086bd836117f85cbeb18211c783209
appinformationprojects <164901181+appinformationprojects@users.noreply.github.com>
2024-04-17 20:48:35 +0530

    Add files via upload

---
+++ Raj Mega World In PHP/frontendcode/js files/SoftDrinks.js
@@ -0,0 +1,43 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.toggle("active");
+  cartForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+}
+
+let cartForm = document.querySelector('.cart-form');
+
+document.querySelector('#cart-btn').onclick = () => {
+  cartForm.classList.toggle("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle("active");
+    searchForm.classList.remove("active");
+    cartForm.classList.remove("active");
+    navbar.classList.remove("active");
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle("active");
+    searchForm.classList.remove("active");
+    cartForm.classList.remove("active");
+    loginForm.classList.remove("active");
+    
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove("active");
+    loginForm.classList.remove("active");
+    navbar.classList.remove("active");
+    cartForm.classList.remove("active");
+}
